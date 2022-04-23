bcc9cb1646f7d8c5b8ce80c5c8813370a49112eb
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-04-23 19:06:02 +0200

    installing pinia and configuring a root pinia store
    `npm install -S pinia@latest`

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import pinia from './stores'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(pinia)
 app.mount('#app')
