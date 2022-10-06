87923e0f9533d64e915df34830a94d3102e6f256
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-10-06 12:19:27 +0200

    removing vuex
    `npm uninstall vuex`

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import pinia from './stores'
 
 const app = createApp(App)
 app.use(router)
-app.use(store)
 app.use(pinia)
 app.mount('#app')
--- src/store/modules/todo-types.js
+++
@@ -1,1 +0,0 @@
-export const GET_TODO_LIST = 'GET_TODO_LIST'
